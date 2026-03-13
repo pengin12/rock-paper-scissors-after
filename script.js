3454c6ae42fc8628c69da669c9ca55429f81901e
pengin12 <137031551+pengin12@users.noreply.github.com>
2026-03-13 15:16:43 +0700

    Add getHumanChoice

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
+
+function getHumanChoice() {
+  choice = prompt("Rock, Paper or Scissors?").toLowerCase();
+
+  if (choice === "rock") {
+    return 0;
+  } else if (choice === "paper") {
+    return 1;
+  } else if (choice === "scissors") {
+    return 2;
+  }
+}
